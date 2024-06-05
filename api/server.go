4d6cdaef1a8a7e43cb72fefc7a309b963b45fdd5
disyudhis <yudhistiraaaa20@gmail.com>
2024-06-05 13:22:43 +0700

    building api (Gin + JWT + Paseto)

---
+++ api/server.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"fmt"
+
+	db "github.com/disyudhis/simplebank/db/sqlc"
+	"github.com/disyudhis/simplebank/token"
+	"github.com/disyudhis/simplebank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
+// Server serves HTTP request for our banking service
+type Server struct {
+	config util.Config
+	store db.Store
+	tokenMaker token.Maker
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config: config, 
+		store: store, 
+		tokenMaker: tokenMaker,
+	}
+	
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+	server.setupRouter()
+	return server, nil
+}
+
+// Start runs the http server on the spesific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.CreateTransfer)
+
+	server.router = router
+}
